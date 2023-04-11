5c48b6d680cd7737aa2a4ef8e8700797553e921c
Lê Phan Duy Tùng <lpduytung@gmail.com>
2023-04-11 09:50:25 +0700

    Add file generate db

---
+++ QLKS_TKPM.sql
@@ -0,0 +1,148 @@
+USE MASTER
+GO
+IF DB_ID('QLKS_TKPM1') IS NOT NULL
+	DROP DATABASE QLKS_TKPM1
+GO
+CREATE DATABASE QLKS_TKPM1
+go
+use QLKS_TKPM1
+CREATE TABLE LOAIPHONG(
+ MaLoaiPhong nvarchar(12) PRIMARY KEY NOT NULL,
+ TenLoaiPhong nvarchar(12),
+ SoLuong int,
+ HoTroDV bit
+)
+
+CREATE TABLE PHONG(
+ MaPhong nvarchar(12) PRIMARY KEY NOT NULL,
+ TenPhong nvarchar(12),
+ LoaiPhong nvarchar(12),
+ GiaThue int,
+ SoNguoi int,
+ TrangThai nvarchar(12)
+)
+
+CREATE TABLE PHIEUTHUEPHONG (
+ MaPhieuThue nvarchar(12) PRIMARY KEY NOT NULL,
+ MaKH nvarchar(12),
+ MaPhongThue nvarchar(12),
+ NgayBD date,
+ NgayKT date,
+ TongTien int
+)
+
+CREATE TABLE HOADON (
+ MaHD nvarchar(12) PRIMARY KEY NOT NULL,
+ MaNV nvarchar(12),
+ PhieuThuePhong nvarchar(12),
+ SoTien int,
+ NgayThanhToan date
+)
+
+CREATE TABLE DANGKYDV (
+ MaPhieuPhong nvarchar(12) NOT NULL,
+ MaDichVu nvarchar (12) NOT NULL,
+ NgayDangKy date,
+ SoLuong int,
+ PRIMARY KEY(MaPhieuPhong,MaDichVu)
+)
+
+CREATE TABLE DICHVU(
+ MaDV nvarchar(12) PRIMARY KEY NOT NULL,
+ TenDV nvarchar(12),
+ DonGia int,
+ SoNguoiSD int
+)
+
+CREATE TABLE SANPHAM(
+ MaSP nvarchar(12) PRIMARY KEY NOT NULL,
+ TenSP nvarchar(12),
+ DonGiaSP int
+)
+
+CREATE TABLE LICHSUMUASP (
+ MaPhieuPhong nvarchar(12) NOT NULL,
+ MaSanPham nvarchar(12) NOT NULL,
+ SoLuong int,
+ NgayMua date
+ PRIMARY KEY(MaPhieuPhong,MaSanPham)
+)
+
+CREATE TABLE ACCOUNT (
+ MaTK nvarchar(12) primary key, 
+ LoaiTK nvarchar(12) not null,
+ MaNV nvarchar(12) not null,
+ Usrname nvarchar(20) not null,
+ Pwd nvarchar(20) not null
+)
+
+CREATE TABLE NHANVIEN (
+ MaNV Nvarchar(12) primary key, 
+ TenNV Nvarchar(20) not null,
+ CMND Nvarchar(20) not null,
+ DiaChi Nvarchar(50) not null,
+ MaTK Nvarchar(12) not null
+)
+
+CREATE TABLE KHACHHANG(
+  MaKH Nvarchar(12) primary key,
+  TenKH Nvarchar(20) not null,
+  NgaySinh date not null,
+  CMND Nvarchar(20) not null,
+  DiaChi Nvarchar(50) not null
+)
+
+CREATE TABLE DOANKHACH (
+ MaDoan nvarchar(12) PRIMARY KEY,
+ TenDoan nvarchar(20),
+ TruongDoan nvarchar(12),
+ SoNguoi int
+)
+CREATE TABLE TOURDULICH(
+    MaTour Nvarchar(12) primary key,
+    TenTour Nvarchar(20) not null,
+    MoTa Nvarchar(50) not null,
+    ThoiLuongTour Nvarchar(20) not null,
+    SDTDoiTac Nvarchar(20) not null
+)
+
+CREATE TABLE DangKyTour(
+ MaKH Nvarchar(12) primary key,
+ MaTour Nvarchar(12) not null,
+ NgayDangKy date not null,
+ YeuCau Nvarchar(50),
+ SoNguoi int not null
+)
+
+CREATE TABLE LICHSUDUNGDV (
+ MaKH nvarchar(12),
+ MaPhieuThue nvarchar(12),
+ MaDV nvarchar(12),
+ ThoiGianSuDung date,
+ CaSuDung time
+ PRIMARY KEY (MaKH,MaPhieuThue,MaDV)
+)
+
+ALTER TABLE PHONG ADD CONSTRAINT fk_phong_loaiphong FOREIGN KEY (LoaiPhong) REFERENCES LOAIPHONG(MaLoaiPhong)
+
+ALTER TABLE LICHSUMUASP ADD CONSTRAINT fk_MuaSP_SanPham FOREIGN KEY (MaSanPham) REFERENCES SANPHAM(MaSP)
+ALTER TABLE LICHSUMUASP ADD CONSTRAINT fk_MuaSP_ThuePhong FOREIGN KEY (MaPhieuPhong) REFERENCES PhIEUTHUEPHONG(MaPhieuThue)
+
+ALTER TABLE PHIEUTHUEPHONG ADD CONSTRAINT fk_ThuePhong_phong FOREIGN KEY (MaPhongThue) REFERENCES PHONG(MaPhong)
+ALTER TABLE PHIEUTHUEPHONG ADD CONSTRAINT fk_ThuePhong_KhachHang FOREIGN KEY (MaKH) REFERENCES KHACHHANG(MaKH)
+
+ALTER TABLE DANGKYDV ADD CONSTRAINT fk_DKDV_ThuePhong FOREIGN KEY (MaPhieuPhong) REFERENCES PHIEUTHUEPHONG(MaPhieuThue)
+ALTER TABLE DANGKYDV ADD CONSTRAINT fk_DKDV_DV FOREIGN KEY (MaDichVu) REFERENCES DICHVU(MaDV)
+
+ALTER TABLE LICHSUDUNGDV ADD CONSTRAINT fk_LichSDDV_KhachHang FOREIGN KEY (MaKH) REFERENCES KHACHHANG(MaKH)
+ALTER TABLE LICHSUDUNGDV ADD CONSTRAINT fk_LichSDDV_DKDV FOREIGN KEY (MaPhieuThue, MaDV) REFERENCES DANGKYDV(MaPhieuPhong,MaDichVu)
+
+ALTER TABLE NHANVIEN ADD CONSTRAINT fk_nhanvien_account FOREIGN KEY (MaTK) REFERENCES ACCOUNT(MaTK)
+
+ALTER TABLE DOANKHACH ADD CONSTRAINT fk_doankhach_khachhang FOREIGN KEY (TruongDoan) REFERENCES KHACHHANG(MaKH)
+
+ALTER TABLE DangKyTour ADD CONSTRAINT fk_dangkytour_khachhang FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH)
+ALTER TABLE DangKyTour ADD CONSTRAINT fk_dangkytour_tourdulich FOREIGN KEY (MaTour) REFERENCES TOURDULICH(MaTour)
+
+ALTER TABLE HOADON ADD CONSTRAINT fk_hoadon_nhanvien FOREIGN KEY (MaNV) REFERENCES NHANVIEN(MaNV)
+ALTER TABLE HOADON ADD CONSTRAINT fk_hoadon_thuephong FOREIGN KEY (PhieuThuePhong) REFERENCES PHIEUTHUEPHONG(MaPhieuThue)
